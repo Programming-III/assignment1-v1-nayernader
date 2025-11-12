37dce35be2bbbef61cbf37eee50c27cd81ebab3b
nayernader <nayernadernono@gmail.com>
2025-11-12 15:25:17 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -7,5 +7,25 @@ using namespace std;
 //define animal class here 
 
 
+class animal{
+    private:
+  string name;
+  int age;
+  bool ishungry;
+  public:
+  animal(string n,int a,bool ih);
+  animal();
+  ~animal();
+  void setname(string n);
+  string getname();
+  void setage(int x);
+  int getage();
+  void sethungry(bool h);
+  bool gethungry();
+  void display();
+  void feed();
+};
+
+
 
 #endif
