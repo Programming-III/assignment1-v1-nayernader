47aa173537c16561c26d6154a397b7e53d05c011
nayernader <nayernadernono@gmail.com>
2025-11-12 15:27:06 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -7,7 +7,26 @@
 
 
 
+class enclosure:animal{
+    private:
+    animal* animal;
+    int capacity;
+    int curretcount;
+    public:
+    enclosure(int c,int cc);
+    enclosure();
+    ~enclosure();
+    void setcapacity(int x);
+     int getcapacity();
+     void setcurretcount(int cc);
+     int getcurrentcount();
+     void addanimal(animal* animal);
+     void displayanimals();
 
+
+
+
+};
 
 
 
